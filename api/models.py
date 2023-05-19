e96024f017cb095d53fca9115637eeda8f614489
moselamar <44118179+moselamar@users.noreply.github.com>
2023-05-19 18:33:11 +0300

    change model to coordinates as in views

--- api/models.py
+++ api/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 
 class Point(models.Model):
-    points = models.CharField(max_length=255)
+    coordinates = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.points
-class Meta:
-        app_label = 'api'
+        return self.coordinates
