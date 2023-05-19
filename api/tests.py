9e516a8905c9850e88a8a77a1ad1216cacb9b9fd
moselamar <44118179+moselamar@users.noreply.github.com>
2023-05-19 18:39:36 +0300

    fixed unittes tissue

--- api/tests.py
+++ api/tests.py
@@ -1,24 +1,16 @@
 from django.test import TestCase
-from django.urls import reverse
-from rest_framework import status
 from rest_framework.test import APIClient
-from .models import Point
+from rest_framework import status
 
 class ClosestPointsAPITest(TestCase):
     def setUp(self):
         self.client = APIClient()
-        self.url = reverse('closest_points')
 
     def test_closest_points_api(self):
-        data = {
-            'points': '2,2;-1,30;20,11;4,5'
-        }
-        response = self.client.post(self.url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(len(response.data['pointss']), 2)
+        input_points = '2,2;-1,30;20,11;4,5'
+        expected_response = ['2,2', '4,5']
 
-        # Check if the points are stored in the database
-        stored_points = Point.objects.all()
-        self.assertEqual(stored_points.count(), 2)
-        self.assertEqual(stored_points[0].points, '2,2')
-        self.assertEqual(stored_points[1].points, '4,5')
+        response = self.client.post('/api/closest-points/', {'points': input_points}, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data, expected_response)
